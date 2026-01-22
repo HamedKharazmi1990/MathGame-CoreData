217aec99c905286116094f9c47197f9bb9b88c73
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-22 15:58:41 +0100

    Added problem model

---
+++ Math Game/Model/ProblemModel.swift
@@ -0,0 +1,45 @@
+//
+//  ProblemModel.swift
+//  Math Game
+//
+//  Created by Hamed Kharazmi on 22.01.26.
+//
+
+import Foundation
+
+struct ProblemModel: Identifiable {
+    let id = UUID()
+    let number1: Int
+    let number2: Int
+    let level: Int
+    
+    // solutions[0] is the answer
+    // the other elements are distractions.
+    let solutions: [Int]
+    
+    var answer: Int {
+        solutions[0]
+    }
+    
+    func checkSolution(value: Int) -> Bool {
+        !solutions.isEmpty && value == answer
+    }
+    
+    init(level: Int) {
+        self.level = level
+        let lower = (level - 1) * 5
+        let upper = level * 5
+        self.number1 = Int.random(in: lower...upper)
+        self.number2 = Int.random(in: lower...upper)
+        let answer = number1 + number2
+        
+        var newSolutions: [Int] = []
+        newSolutions.append(answer) // True solution at solutions[0] = 27
+        newSolutions.append(answer + 10) // solutions[1] Distraction 37
+        let belowAnswer = max(0, answer - 5)
+        let aboveAnswer = answer + 5
+        newSolutions.append(Int.random(in: answer+1...aboveAnswer)) // solutions[2]
+        newSolutions.append(Int.random(in: belowAnswer..<answer)) // solutions[3]
+        self.solutions = newSolutions
+    }
+}
