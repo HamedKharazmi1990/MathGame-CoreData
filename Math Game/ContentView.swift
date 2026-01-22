e542dec0219b7758a13aa93bb44920bff12681f1
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-22 13:04:18 +0100

    Initial Commit

---
+++ Math Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Math Game
+//
+//  Created by Hamed Kharazmi on 22.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Math Game/Math_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_GameApp.swift
+//  Math Game
+//
+//  Created by Hamed Kharazmi on 22.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Math_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
