0462a2e7ed2b86e09acd7d3015872b2bf7b8acbd
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-22 13:49:04 +0100

    Added asset images and create image constant

---
+++ Math Game/Constants/Image Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Image Constants.swift
+//  Math Game
+//
+//  Created by Hamed Kharazmi on 22.01.26.
+//
+
+import Foundation
+
+enum Creatures: String {
+    case creature0, creature1, creature2, creature3, creature4, creature5
+}
