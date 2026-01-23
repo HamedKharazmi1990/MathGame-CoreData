232f46239b42ee1912951b6e95a65af2d23781c0
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-23 17:39:06 +0100

    Implemented edit mode in high score view

--- Math Game/View/High Score View/HighScoreView.swift
+++ Math Game/View/High Score View/HighScoreView.swift
@@ -19,8 +19,12 @@ struct HighScoreView: View {
                 HighScoreTitle()
                 
                 List {
-                    ForEach(highScoreVM.highScores) { entity in
-                        Text("\(entity.score)")
+                    ForEach(Array(highScoreVM.highScores.enumerated()), id: \.offset) { index, entity in
+                        RankScoreView(
+                            rank: index + 1,
+                            score: Int(entity.score),
+                            entity: entity
+                        )
                     }
                 }
                 
@@ -28,6 +32,62 @@ struct HighScoreView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100, height: 100)
+            }
+        }
+    }
+}
+
+struct RankScoreView: View {
+    let rank: Int
+    let score: Int
+    let entity: HighScoreEntity
+    var color: Color {
+        scoreColors[(rank - 1) % scoreColors.count]
+    }
+    let scoreColors: [Color] = [
+        .blue, .pink, .purple, .yellow, .orange
+    ]
+    @State private var editMode = false
+    
+    @Environment(HighScoreViewModel.self) private var highScoreVM
+    
+    @State private var name: String = ""
+    @State private var save = false
+    
+    var body: some View {
+        VStack {
+            if editMode {
+                HStack {
+                    TextField(entity.name ?? "Name", text: $name)
+                        .padding()
+                        .background(Color.green.gradient)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                        .cornerRadius(10)
+                    
+                    Button {
+                        highScoreVM.updateHighScore(
+                            entity: entity,
+                            name: name.isEmpty ? (entity.name ?? "Anon") : name)
+                        withAnimation {
+                            editMode.toggle()
+                        }
+                    } label: {
+                        Text("Save")
+                            .foregroundStyle(.white)
+                            .padding()
+                            .background(Color.red.gradient)
+                            .cornerRadius(10)
+                    }
+
+                }
+            } else {
+                Text("Display score data")
+                    .onTapGesture {
+                        withAnimation {
+                            editMode.toggle()
+                        }
+                    }
             }
         }
     }
