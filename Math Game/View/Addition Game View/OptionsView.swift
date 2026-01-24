3521b73806ed8d6194a31d67f11cc6332add0ce1
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-24 15:21:13 +0100

    Added bounce to option buttons

--- Math Game/View/Addition Game View/OptionsView.swift
+++ Math Game/View/Addition Game View/OptionsView.swift
@@ -17,25 +17,67 @@ struct OptionsView: View {
     var body: some View {
         LazyVGrid(columns: columns) {
             ForEach(gameVM.possibleSolutions, id: \.self) { option in
-                // Use it in debug mode and apply it to BuubleView(textColor: cheatColor)
+                SelectButton(option: option, gameVM: gameVM)
+            }
+        }
+    }
+}
+
+struct SelectButton: View {
+    let option: Int
+    @State var selectedOption: Int? = nil
+    let gameVM: AdditionGameViewModel
+    
+    var isSelected: Bool {
+        selectedOption == option
+    }
+    
+    var scale: CGFloat {
+        isSelected ? 1.2 : 1
+    }
+    
+    var color: Color {
+        isSelected ? getRandomColor() : .purple.opacity(0.7)
+    }
+    
+    func getRandomColor() -> Color {
+        [.green.opacity(0.7),
+         .red,
+         .blue.opacity(0.5)]
+            .randomElement() ?? .green.opacity(0.7)
+    }
+    var body: some View {
+        // Use it in debug mode and apply it to BuubleView(textColor: cheatColor)
 //                let cheatColor: Color = option == gameVM.answer ? .red : .black
-                
-                BubbleView(
-                    textColor: .black,
-                    bgColor: .purple.opacity(0.7),
-                    name: "bubble2",
-                    text: ("\(option)")
-                )
-                .onTapGesture {
-                    withAnimation {
-                        if gameVM.answer == option {
-                            gameVM.increseScore()
-                        } else {
-                            gameVM.loseLife()
-                        }
-                        gameVM.generateNumbers()
-                    }
+        
+        BubbleView(
+            textColor: .black,
+            bgColor: color,
+            name: "bubble2",
+            text: ("\(option)")
+        )
+        .scaleEffect(scale)
+        .onTapGesture {
+            withAnimation(
+                .spring(response: 0.5, dampingFraction: 0.3)
+                .repeatCount(1, autoreverses: true)
+            ) {
+                selectedOption = option
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                withAnimation {
+                    selectedOption = nil
                 }
+            }
+            
+            withAnimation {
+                if gameVM.answer == option {
+                    gameVM.increseScore()
+                } else {
+                    gameVM.loseLife()
+                }
+                gameVM.generateNumbers()
             }
         }
     }
